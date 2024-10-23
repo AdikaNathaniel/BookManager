42f248f9d4f61e47059247cf862f0f292c8cb620
AdikaNathaniel <128187291+AdikaNathaniel@users.noreply.github.com>
2024-10-23 13:43:03 +0000

    Newest Commit

---
+++ BaseLibrary/DTOs/RefreshToken.cs
@@ -0,0 +1,11 @@
+﻿
+
+namespace BaseLibrary.DTOs
+{
+    public  class RefreshToken
+    {
+        public string? Token { get; set; }
+
+
+    }
+}
--- BaseLibrary/Entities/ApplicationUser.cs
+++ BaseLibrary/Entities/ApplicationUser.cs
@@ -5,7 +5,7 @@ namespace BaseLibrary.Entities
     {
         public int Id { get; set; }
 
-        public string? Name { get; set; }
+        public string? Fullname { get; set; }
 
         public string? Email { get; set; }
 
