48c352a8f8c06b7456e3e27c923ce720befba3c2
AdikaNathaniel <128187291+AdikaNathaniel@users.noreply.github.com>
2024-10-23 02:48:19 +0000

    New

---
+++ BaseLibrary/Entities/BaseEntity.cs
@@ -0,0 +1,20 @@
+﻿
+using System.Text.Json.Serialization;
+
+namespace BaseLibrary.Entities
+{
+    public  class BaseEntity
+    {
+
+        public int Id { get; set; }
+
+        public string? Name { get; set; }
+
+        //Relationship of one to many
+        //Meaning one branch or town or any of the entities above can have many employees.
+        [JsonIgnore]
+        public List<Employee>? Employees { get; set; }
+
+
+    }
+}
---
+++ BaseLibrary/Entities/Relationship.cs
@@ -0,0 +1,13 @@
+﻿
+
+using System.Text.Json.Serialization;
+
+namespace BaseLibrary.Entities
+{
+    public class Relationship
+    {
+
+       
+
+    }
+}
